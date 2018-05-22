f312d7a553d7a929816223eff7acac018dd4bdf8
pradeepa <pradeepa@pradeepas-iMac.local>
2018-05-22 14:36:08 +1000

    deleted all the content and added new ph number

--- wp-content/themes/corgan-child/templates/contact-info-cart.php
+++ wp-content/themes/corgan-child/templates/contact-info-cart.php
@@ -1,12 +1,6 @@
-<?php
-$cart_items = WC()->cart->get_cart_contents_count();
-$cart_summa = strip_tags(WC()->cart->get_cart_subtotal());
-?>
-<div class="top_panel_cart_button">
-	<div class="contact_cart-totals">
-		<span>Call Us:</span> <a href="tel:0401856208">0401 856 208</a>
-	</div>
-	<div class="contact_cart_totals">
-		<span class="cart_items">Email: </span><a href="mailto:wc.ceilings@gmail.com" style="color: #000 !important;">wc.ceilings@gmail.com</a>
-	</div>
+<div class="header-contact-section">
+	<span style="color: #000 !important;">Call Us: </span><a href="tel:0401856208">0401 856 208</a><br>
+	<span style="color: #000 !important;">Email: </span><a href="mailto:wc.ceilings@gmail.com">wc.ceilings@gmail.com</a>
 </div>
+
+
